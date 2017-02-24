97134f94936df0a165ed2828c43f9cb71e8a4a29
Adam Yala <adamyala@mit.edu>
2017-02-24 10:41:40 -0500

    debug

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 import cPickle as p 
 from PIL import Image
-
+import random
 def buildgraph(): 
 	return 0
 
@@ -14,9 +14,16 @@ class Reader:
     lines = []
     with open(path, "rb") as file:
       lines = file.readlines()
-    self.x,self.y = [(l.split()[0]) for l in lines],[(l.split()[1]) for l in lines]
-    print self.x[0]
-    print self.y[0]
+    
+    random.shuffle(lines)
+
+    self.all_x,self.all_y = [(l.split()[0]) for l in lines],[(l.split()[1]) for l in lines]
+    self.x = self.all_x[:7000]
+    self.y = self.all_y[:7000]
+
+    self.dev_x = self.all_x[7000:8000]
+    self.dev_y = self.all_y[7000:8000]
+    self.dev_i = 0
 
   def getBatch(self):
     def oneHot(s): 
@@ -30,23 +37,27 @@ class Reader:
       return np.asarray(im, dtype="float32")
       
     indices = np.random.choice(range(minibatch), minibatch)
-    images = np.array([nameToImage("../images/" + self.x[i]) for i in indices])
+    images = np.array([nameToImage("images/" + self.x[i]) for i in indices])
     labels = np.array([oneHot(self.y[i]) for i in indices])
     return (images,labels)
+ 
 
 class CNN: 
   def __init__(self):
-    self.x = tf.placeholder(tf.float32, shape=[minibatch,200,200,3])
-    self.y = tf.placeholder(tf.int32, shape=[minibatch,3])
-    self.keep_prob = tf.placeholder(tf.float32)    
 
     filters = [1,2,3,5,7]
     numFilters = 32
+    numIn = 4
     pools = []
+    
+
+    self.x = tf.placeholder(tf.float32, shape=[minibatch,200,200,numIn])
+    self.y = tf.placeholder(tf.int32, shape=[minibatch,3])
+    self.keep_prob = tf.placeholder(tf.float32)    
 
     for filter in filters:
       with tf.name_scope("conv" + str(filter)):
-        filterShape = [filter, filter, 3, numFilters]
+        filterShape = [filter, filter, numIn, numFilters]
         W = tf.Variable(tf.truncated_normal(filterShape, stddev = .1), name="W")
         b = tf.Variable(tf.constant(.1, shape=[numFilters], name="b"))
         conv = tf.nn.conv2d(self.x, W, strides=[1,1,1,1], padding="VALID", name="conv")
@@ -61,26 +72,28 @@ class CNN:
     softmax_w = tf.Variable(tf.truncated_normal( [hsize  , 3], stddev=.1 ), name="softmax_w")
     softmax_b = tf.Variable(tf.constant(.1, shape=[3]), name="softmax_b")
     logits = tf.matmul( h_drop, softmax_w ) + softmax_b
-    print logits
 
     self.loss =  tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(logits, self.y, name="softmax_op") )
     
     self.opt  = tf.train.AdamOptimizer().minimize(self.loss)
  
 def main(_):
-    r = Reader("../images/labels.txt")
-    images, labels = r.getBatch()
+    r = Reader("images/labels.txt")
+    cnn = CNN()
     maxSteps = 10000
-    with tf.Session() as sess:
-	cnn = CNN()
-	sess.run ( tf.global_variables_initializer())
-        for step in range(maxSteps):
-	    batch_x, batch_y = r.getBatch()
-	    feed_dict = { cnn.x: batch_x, cnn.y: batch_y, cnn.keep_prob:.75}
-	    fetch = { "loss":cnn.loss, "opt":cnn.opt}
-	    res = sess.run(fetch, feed_dict=feed_dict)
-	    if step % 100 == 0:
-		print "step:", step, "loss:", res['loss']
+    sv = tf.train.Supervisor(logdir="logs", summary_op=None)
+    with sv.managed_session() as sess:
+	   # sess.run ( tf.global_variables_initializer())
+
+            for step in range(maxSteps):
+	        batch_x, batch_y = r.getBatch()
+	        feed_dict = { cnn.x: batch_x, cnn.y: batch_y, cnn.keep_prob:.75}
+	        fetch = { "loss":cnn.loss, "opt":cnn.opt}
+	        res = sess.run(fetch, feed_dict=feed_dict)
+	        if step % 54 == 0:
+                    epoch = step/54
+	  	    print "epoch:", epoch, "loss:", res['loss']
+                    sv.saver.save(sess, 'inferProgram', global_step=epoch)
 
 if (__name__ == "__main__"): 
   tf.app.run()
